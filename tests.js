2105a14b8ed2fdca70004670f300d0156153e686
koyexes <koyagabriel@gmail.com>
2016-08-18 11:56:19 +0100

    Made changes

--- tests.js
+++ tests.js
@@ -6,11 +6,21 @@ var assert = chai.assert;
 var NotesApplication = require('./lib/notesapplication.js');
 var Note = require('./lib/note.js');
 
+
 describe("Note creation works properly", function() {
     it("assigns author based on the parameter supplied in the constructor", function() {
-        note = new Note("Hello world", "Chidiebere")
-        assert(note.author == "Chidiebere")
+        note = new Note("Hello world", "koya")
+        assert(note.author == "koya")
     })
+    it("assigns text to note based on the parameter supplied in the constructor", function() {
+        note = new Note("Hello world", "koya")
+        assert(note.text == "Hello world")
+    })
+    it("asserts that empty parameters are not passed in", function () {
+        note = new Note("Hello World", "koya")
+        assert( (note.text != "") || (note.author != ""))
+    })
+    it("asserts that an empty parameter isn't passed into the note set")
 })
 
 describe("Notes application increments number of notes as notes are added", function() {

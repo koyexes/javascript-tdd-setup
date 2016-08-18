6c336ea605979694f4adb85c22a7ab5ebb2fe183
koyexes <koyagabriel@gmail.com>
2016-08-18 12:19:07 +0100

    Updated my test cases

--- tests.js
+++ tests.js
@@ -4,7 +4,7 @@ var chai = require('chai');
 var assert = chai.assert;
 
 var NotesApplication = require('./lib/notesapplication.js');
-var Note = require('./lib/note.js');
+var Note = require('./lib/notes.js');
 
 
 describe("Note creation works properly", function() {
@@ -20,13 +20,18 @@ describe("Note creation works properly", function() {
         note = new Note("Hello World", "koya")
         assert( (note.text != "") || (note.author != ""))
     })
-    it("asserts that an empty parameter isn't passed into the note set")
+    it("asserts that an empty parameter isn't passed into the setNote method"), function() {
+        note = new Note("Hello World", "koya")
+        note.setNote("Made changes") 
+        assert( note.text == "Made changes")
+        
+    })
 })
 
 describe("Notes application increments number of notes as notes are added", function() {
     it("increments the note list as notes are added", function() {
-        note = new Note("Hello world", "Chidiebere");
-        noteapp = new NotesApplication("Chidiebere");
+        note = new Note("Hello world", "koya");
+        noteapp = new NotesApplication("koya");
         assert(noteapp.notelist.length == 0)
         noteapp.addNote(note)
         assert(noteapp.notelist.length == 1)
